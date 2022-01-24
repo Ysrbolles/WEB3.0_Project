a8bc094d1b5b164b36f29dcff504fafc3b37ff49
Yassir BOLLES <ysrbolles@gmail.com>
2022-01-24 20:21:29 +0100

    ✅ Finished Navbar part

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { HiMenuAlt4 } from "react-icons/hi"
 import { AiOutlineClose } from 'react-icons/ai'
 
 import logo from '../../images/logo.png'
+import { useState } from "react"
 
 const NavbarItem = ({ title, classNameProps }) => {
 	return (
@@ -12,6 +13,8 @@ const NavbarItem = ({ title, classNameProps }) => {
 }
 
 const Navbar = () => {
+	const [toggleMenu, setToggleMenu] = useState(false)
+
 	return (
 		<nav className="w-full flex md:justify-center justify-between items-center p-4">	<div className="md:flex-[0.5] flex-initial justify-center items-center ">
 			<img src={logo} alt="logo" className="w-20  cursor-pointer" />
@@ -20,10 +23,26 @@ const Navbar = () => {
 				{["Market", "Exchange", "Wallet"].map((item, index) => {
 					return (<NavbarItem key={item + index} title={item} />)
 				})}
-				<li className="bg-[#2952e3] py-2 px-6 mx-4 cursor-pointer rounded-full" >Login</li>
+				<li className="bg-[#2952e3] py-2 px-6 mx-4 cursor-pointer rounded-full hover:bg-[#2546bd]" >Login</li>
 			</ul>
+			<div className="flex relative  md:hidden">
+				{
+					toggleMenu ?
+						<AiOutlineClose fontSize={20} className="text-white cursor-pointer" onClick={() => setToggleMenu(false)} /> :
+						<HiMenuAlt4 fontSize={20} className="text-white cursor-pointer" onClick={() => setToggleMenu(true)} />
+				}
+				{toggleMenu && <ul className="z-1 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none 
+				flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
+					<li className="text-xl w-full my-2">
+						<AiOutlineClose onClick={() => setToggleMenu(false)} />
+					</li>
+					{["Market", "Exchange", "Wallet"].map((item, index) => {
+						return (<NavbarItem key={item + index} title={item} classNameProps="my-2 text-lg" />)
+					})}
+				</ul>}
+			</div>
 		</nav>
 	)
 }
 
-export default Navbar
+export default Navbar 
